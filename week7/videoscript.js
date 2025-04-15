fd2fe21785eea70f4e901cf978bfb6117d6c733f
jannalyntan <s4056775@student.rmit.edu.au>
2025-04-15 16:14:32 +1000

    week 7

--- week7/videoscript.js
+++ week7/videoscript.js
@@ -12,7 +12,7 @@ console.log(playButton);
 // playing sound on click
 playButton.addEventListener("click", playAudio);
 
-//play my logic
+// my play logic
 function playAudio() {
   myVideo.play();
 }
@@ -27,8 +27,34 @@ console.log(pauseButton);
 // playing sound on click
 pauseButton.addEventListener("click", pauseAudio);
 
-// pause my logic
+//my pause logic
 function pauseAudio() {
   myVideo.pause();
 }
+//----------------------------------------------------
+
+//----------------------------------------------------
+//here is my logic for playing and pausing the video with a single button
+//fiirst I am fetching the right play, pause button
+const playpauseButton = document.querySelector("#play-pause-button");
+console.log(playpauseButton);
+
+//pausing with click
+playpauseButton.addEventListener("click", toggleVideo);
+
+const playPauseImg = document.querySelector("#play-pause-img");
+console.log(playPauseImg);
+
+//my pauce logic
+function toggleVideo() {
+  if (myVideo.paused || myVideo.ended) {
+    myVideo.play();
+    // changing the icon based on whether the video is playing or pausing
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v2.png";
+  } else {
+    myVideo.pause();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v2.png";
+  }
+}
+
 //----------------------------------------------------
